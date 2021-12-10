822c3fa4ed59fdb012023d8bfbf2f74798bd45fc
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-12-09 22:17:28 -0300

    Desenhando pacman.

---
+++ src/pacman.py
@@ -0,0 +1,47 @@
+import pygame
+
+pygame.init()
+
+tela = pygame.display.set_mode((800, 600), 0)
+
+amarelo = (255,255,0)
+preto = (0,0,0)
+
+class Pacman:
+    def __init__(self):
+        self.centro_x = 400
+        self.centro_y = 300
+        self.tamanho = 100
+        self.raio = self.tamanho/2
+
+    def pintar(self,tela):
+        # desenhando corpo pacman
+        pygame.draw.circle(tela, amarelo, (self.centro_x, self.centro_y), self.raio, 0)
+
+        #Desenho boca pacman
+        canto_boca = (self.centro_x, self.centro_y)
+        labio_superior = (self.centro_x + self.raio, self.centro_y - self.raio)
+        labio_inferior = (self.centro_x + self.raio, self.centro_y)
+
+        pontos_boca_pacman = [canto_boca, labio_superior, labio_inferior]
+
+        pygame.draw.polygon(tela, preto, pontos_boca_pacman, 0)
+
+        #Desenho olho Pacman
+        olho_x = int(self.centro_x + (self.raio / 3))
+        olho_y = int(self.centro_y - (self.raio * 0.70))
+        olho_raio = int(self.raio / 10)
+        pygame.draw.circle(tela, preto, (olho_x, olho_y), olho_raio, 0)
+
+if __name__ == "__main__":
+    pacman = Pacman()
+
+
+while True:
+
+    pacman.pintar(tela)
+    pygame.display.update()
+    
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            exit()
